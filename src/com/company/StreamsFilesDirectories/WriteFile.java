37827723b9214d1d59e0dfad3a6d435ff4e6e3bf
Raiko Dimitrov <98freddy98@gmail.com>
2024-11-12 01:58:57 +0200

    Added new tasks and text files on Streams, Files and Directories.

--- src/com/company/StreamsFilesDirectories/WriteFile.java
+++ src/com/company/StreamsFilesDirectories/WriteFile.java
@@ -1,7 +1,6 @@
 package com.company.StreamsFilesDirectories;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
@@ -10,15 +9,13 @@ import java.util.Set;
 public class WriteFile {
     public static void main(String[] args) {
         String input = "D:\\Coding projects\\Java\\JavaAdvanced\\src\\FilesAndStreams\\input.txt";
-        String output = "D:\\Coding projects\\Java\\JavaAdvanced\\src\\FilesAndStreams\\02.WriteToFileOutput.txt";
         Set<Character> forbiddenSymbols = new HashSet<>();
         Collections.addAll(forbiddenSymbols, '!', '?', ',', '.');
-        try (FileInputStream inputStream = new FileInputStream(input); FileOutputStream outputStream = new FileOutputStream(output)) {
+        try (FileInputStream inputStream = new FileInputStream(input)) {
             int oneByte = inputStream.read();
             while (oneByte >= 0) {
                 char byteAsChar = (char) oneByte;
                 if (!forbiddenSymbols.contains(byteAsChar)) {
-                    outputStream.write(byteAsChar);
                     System.out.printf("%s", byteAsChar);
                 }
                 oneByte = inputStream.read();
